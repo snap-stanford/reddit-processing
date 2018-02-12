b6625329454318c3c3f182ad9ba33c46ad231d57
Jon Deaton <jonpauldeaton@gmail.com>
2018-02-12 13:55:35 -0800

    Trying to find Snap with CMake

---
+++ FindSnap.cmake
@@ -0,0 +1,33 @@
+# - Find the Snap (Stanford Network Analysis Platform) Package
+# Will define
+#  Snap_FOUND        - Snap was found on the system
+#  Snap_INCLUDE_DIRS - The Snap include directories
+#  Snap_LIBRARIES    - The libraries needed to use Snap
+#  Snap_DEFINITIONS  - Compiler switches required for using Snap
+
+set (Snap_ROOT_DIR $ENV{WORKSPACE_ROOT}/opt/Snap)
+MESSAGE(STATUS "Snap_ROOT_DIR: " ${Snap_ROOT_DIR}) # print which directory CMake is looking in.
+
+find_path(Snap_INCLUDE_DIR
+        NAMES Snap
+        PATHS ${Snap_ROOT_DIR}/snap-core ${Snap_ROOT_DIR}/snap-adv ${Snap_ROOT_DIR}/snap-exp
+        DOC "The Snap include directory")
+
+find_library(Snap_LIBRARY
+        NAMES Snap
+        PATHS ${Snap_ROOT_DIR}/snap-core/libsnap.a
+        DOC "The Snap library")
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LOGGING_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(Snap DEFAULT_MSG Snap_INCLUDE_DIR Snap_LIBRARY)
+
+if (Snap_FOUND)
+    set(Snap_LIBRARIES ${Snap_LIBRARY})
+    set(Snap_INCLUDE_DIRS ${Snap_INCLUDE_DIR})
+    set(Snap_DEFINITIONS)
+endif()
+
+# Tell cmake GUIs to ignore the "local" variables.
+mark_as_advanced(Snap_ROOT_DIR Snap_INCLUDE_DIR Snap_LIBRARY)
---
+++ Snap/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Jonathan Deaton on 2/12/18.
+//
+
+#include <Snap.h>
+
+
+int main(int argc, char* argv[]) {
+
+}
