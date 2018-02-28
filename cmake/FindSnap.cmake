16eb4caee5390d80edf3f309f9f0735927398d27
Jon Deaton <jonpauldeaton@gmail.com>
2018-02-28 15:25:49 -0800

    Converted to capital

--- cmake/FindSnap.cmake
+++ cmake/FindSnap.cmake
@@ -1,44 +1,44 @@
 # - Find the Snap (Stanford Network Analysis Platform) Package
 # Will define
-#  Snap_FOUND        - Snap was found on the system
-#  Snap_INCLUDE_DIRS - The Snap include directories
-#  Snap_LIBRARIES    - The libraries needed to use Snap
-#  Snap_DEFINITIONS  - Compiler switches required for using Snap
+#  SNAP_FOUND        - Snap was found on the system
+#  SNAP_INCLUDE_DIRS - The Snap include directories
+#  SNAP_LIBRARIES    - The libraries needed to use Snap
+#  SNAP_DEFINITIONS  - Compiler switches required for using Snap
 include(FindPackageHandleStandardArgs)
 
-set(Snap_ROOT_DIR $ENV{WORKSPACE_ROOT}/opt/Snap)
-message(STATUS "Snap_ROOT_DIR: " ${Snap_ROOT_DIR}) # print which directory CMake is looking in.
+set(SNAP_ROOT_DIR $ENV{WORKSPACE_ROOT}/opt/Snap)
+message(STATUS "SNAP_ROOT_DIR: " ${SNAP_ROOT_DIR}) # print which directory CMake is looking in.
 
-find_path(Snap_CORE
+find_path(SNAP_CORE
         NAMES "Snap.h"
         PATH_SUFFIXES snap-core
-        HINTS ${Snap_ROOT_DIR}
+        HINTS ${SNAP_ROOT_DIR}
         DOC "The Snap include directory")
-message(STATUS "Snap core: " ${Snap_CORE})
+message(STATUS "Snap core: " ${SNAP_CORE})
 
-find_path(Snap_GLIB_CORE
+find_path(SNAP_GLIB_CORE
         NAMES "base.h"
         PATH_SUFFIXES glib-core
-        HINTS ${Snap_ROOT_DIR}
+        HINTS ${SNAP_ROOT_DIR}
         DOC "The Snap GLib include directory")
-message(STATUS "Glib core: " ${Snap_GLIB_CORE})
+message(STATUS "Glib core: " ${SNAP_GLIB_CORE})
 
-find_library(Snap_LIBRARY
+find_library(SNAP_LIBRARY
         NAMES libsnap.a
-        PATHS ${Snap_ROOT_DIR}/snap-core
+        PATHS ${SNAP_ROOT_DIR}/snap-core
         DOC "The Snap library")
-message(STATUS "Snap Library: " ${Snap_LIBRARY})
+message(STATUS "Snap Library: " ${SNAP_LIBRARY})
 
-find_package_handle_standard_args(Snap DEFAULT_MSG
-        Snap_CORE
-        Snap_GLIB_CORE
-        Snap_LIBRARY)
+find_package_handle_standard_args(SNAP DEFAULT_MSG
+        SNAP_CORE
+        SNAP_GLIB_CORE
+        SNAP_LIBRARY)
 
-if (Snap_FOUND)
-    set(Snap_LIBRARIES ${Snap_LIBRARY})
-    set(Snap_INCLUDE_DIRS ${Snap_CORE} ${Snap_GLIB_CORE})
-    set(Snap_DEFINITIONS)
+if (SNAP_FOUND)
+    set(SNAP_LIBRARIES ${SNAP_LIBRARY})
+    set(SNAP_INCLUDE_DIRS ${SNAP_CORE} ${SNAP_GLIB_CORE})
+    set(SNAP_DEFINITIONS)
 endif()
 
 # Tell cmake GUIs to ignore the "local" variables.
-mark_as_advanced(Snap_ROOT_DIR Snap_INCLUDE_DIR Snap_LIBRARY)
+mark_as_advanced(SNAP_ROOT_DIR SNAP_INCLUDE_DIR SNAP_LIBRARY)
