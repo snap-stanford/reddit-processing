d94e567bc49dc30114a62bee9302e546cda784ca
Jon Deaton <jonpauldeaton@gmail.com>
2018-03-03 17:14:17 -0800

    Small fixes

--- scripts/convert.py
+++ scripts/convert.py
@@ -7,15 +7,18 @@ logging.basicConfig(format='[%(asctime)s][%(levelname)s][%(funcName)s] - %(messa
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+
 def convert_csv_to_tsv(csv_in, tsv_out):
-	logger.info("Converting: %s ..." % os.path.split(csv_in)[1])
-	with open(csv_in, 'r') as fin:
-		with open(tsv_out, 'w') as fout:
-			csv.writer(fout, dialect='excel-tab').writerows(csv.reader(fin))
-	logger.info("Completed %s" % os.path.split(csv_in)[1])
+    logger.info("Converting: %s ..." % os.path.split(csv_in)[1])
+    with open(csv_in, 'r') as fin:
+        with open(tsv_out, 'w') as fout:
+            csv.writer(fout, dialect='excel-tab').writerows(csv.reader(fin))
+    logger.info("Completed %s" % os.path.split(csv_in)[1])
+
 
 def convert_csv_to_tsv_unpack(args):
-	convert_csv_to_tsv(*args)
+    convert_csv_to_tsv(*args)
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="CSV to TSV converter", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -24,7 +27,7 @@ def parse_args():
     io_options_group.add_argument('-i', "--input", help="Input directory")
     io_options_group.add_argument('-o', "--output", help="Output directory")
 
-    options_group = parser.add_argument_group("I/O Options")
+    options_group = parser.add_argument_group("Options")
     options_group.add_argument('-p', '--pool-size', type=int, default=10, help="Thread pool size")
 
     console_options_group = parser.add_argument_group("Console Options")
@@ -46,37 +49,37 @@ def parse_args():
 
 
 def main():
-	args = parse_args()
+    args = parse_args()
 
-	logger.debug("Input directory: %s" % args.input)
-	if not os.path.exists(args.input):
-		logger.error("Input directory: %s not found." % args.input)
-		raise
+    logger.debug("Input directory: %s" % args.input)
+    if not os.path.exists(args.input):
+        logger.error("Input directory: %s not found." % args.input)
+        raise Exception()
 
-	if not os.path.exists(args.output):
-		logger.debug("Output directory: %s did not exist. Creating it..." % args.output)
-		os.makedirs(args.output)
-	else:
-		logger.debug("Output directory: %s" % args.output)
+    if not os.path.exists(args.output):
+        logger.debug("Output directory: %s did not exist. Creating it..." % args.output)
+        os.makedirs(args.output)
+    else:
+        logger.debug("Output directory: %s" % args.output)
 
-	input_csvs = [file for file in os.listdir(args.input) if file.endswith(".csv")]
-	output_csvs = ["%s.tsv" % os.path.splitext(file)[0] for file in input_csvs]
+    input_csvs = [file for file in os.listdir(args.input) if file.endswith(".csv")]
+    output_csvs = ["%s.tsv" % os.path.splitext(file)[0] for file in input_csvs]
 
-	input_csvs = map(lambda p: os.path.join(args.input, p), input_csvs)
-	output_csvs = map(lambda p: os.path.join(args.output, p), output_csvs)
+    input_csvs = map(lambda p: os.path.join(args.input, p), input_csvs)
+    output_csvs = map(lambda p: os.path.join(args.output, p), output_csvs)
 
-	num_files = len(input_csvs)
+    num_files = len(input_csvs)
 
-	if input_csvs:
-		logger.info("Found %d CSVs to convert" % num_files)
-	else:
-		logger.warning("Found no CSV files to convert. Exiting.")
-		return
+    if input_csvs:
+        logger.info("Found %d CSVs to convert" % num_files)
+    else:
+        logger.warning("Found no CSV files to convert. Exiting.")
+        return
 
-	logger.debug("Processing with Thread-Pool of size %d" % args.pool_size)
-	pool = mp.Pool(args.pool_size)
-	pool.map(convert_csv_to_tsv_unpack, zip(input_csvs, output_csvs))
-	logger.debug("Conversion complete.")
+    logger.debug("Processing with Thread-Pool of size %d" % args.pool_size)
+    pool = mp.Pool(args.pool_size)
+    pool.map(convert_csv_to_tsv_unpack, zip(input_csvs, output_csvs))
+    logger.debug("Conversion complete.")
 
 if __name__ == "__main__":
-	main()
+    main()
