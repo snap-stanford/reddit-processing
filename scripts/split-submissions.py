dc2d1f88ba01ea2886b612327760b71d2aa94f6d
Jon Deaton <jonpauldeaton@gmail.com>
2018-03-05 13:49:17 -0800

    hi

---
+++ scripts/split-submissions.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+import os, sys, csv
+import logging, argparse
+import hashlib
+import multiprocessing as mp
+import pandas as pd
+
+input_directory = ""
+output_directory = ""
+num_splits = 1024
+pool_size = 20
+target_directories = {}
+compress = False
+
+
+def hash(s):
+    return int(hashlib.md5(s.encode()).hexdigest(), 16)
+
+
+def get_bucket(s):
+    return hash(s) % num_splits
+
+
+def listdir(directory):
+    return list(map(lambda d: os.path.join(directory, d), os.listdir(directory)))
+
+
+def split_by_submission():
+    logger.debug("Creating target directories...")
+    create_target_directories()
+    logger.debug("Target directories created.")
+
+
+    data_sets = listdir(input_directory)
+    comment_data_set = [dir for dir in data_sets if 'comment' in dir][0]
+    submission_data_set = [dir for dir in data_sets if 'submission' in dir][0]
+
+    comment_base_mapping = split_comment_data(comment_data_set, target_directories)
+
+
+
+    # split_data_set('post_fullname', input_directory, )
+
+
+def split_comment_data(comment_data_set_directory, split_target_dir_mapping):
+
+    comment_base_mapping = {}
+    for table_file_name in os.listdir(comment_data_set_directory):
+        table_file_path = os.path.join(comment_data_set_directory, table_file_name)
+
+        logger.debug("Reading comments: %s" % table_file_name)
+        df = pd.read_csv(table_file_path, engine='python')
+
+        logger.debug("Updating target -> submission mapping: %s" % table_file_name)
+        comment_base_mapping.update(dict(zip(df.comment_fullname, df.post_fullname)))
+
+        logger.debug("Splitting: %s" % table_file_name)
+        output_file_map = {i: os.path.join(split_target_dir_mapping[i], table_file_name) for i in range(num_splits)}
+        split_data_frame(df, "post_fullname", get_bucket, output_file_map, compress=compress)
+
+    return comment_base_mapping
+
+
+def split_data_frame(df, on, assign_split, output_file_map, temp_col='bkt', compress=False):
+    df[temp_col] = df[on].apply(assign_split)
+    for i in output_file_map:
+        df_out = df[df[temp_col] == i].drop(temp_col, axis=1)  # select rows and drop temporary column
+        df_out.to_csv(output_file_map[i], index=False, compression='gzip' if compress else None)
+
+
+
+def split_all_data_sets(on, include=None, exclude=None):
+    logger.debug("Creating target directories...")
+    create_target_directories()
+    logger.debug("Target directories created.")
+
+    data_sets = os.listdir(input_directory)
+    for data_set in data_sets:
+        if include and data_set not in include: continue
+        if exclude and data_set in exclude: continue
+        data_set_dir = os.path.join(input_directory, data_set)
+        if not os.path.isdir(data_set_dir): continue
+        logger.info("Splitting data-set: %s" % data_set)
+        split_data_set(on, data_set_dir, data_set)
+
+
+def split_data_set(on, data_set_path, sub_dir_name):
+    targets = {}
+    for i in range(num_splits):
+        targets[i] = os.path.join(target_directories[i], sub_dir_name)
+        if not os.path.isdir(targets[i]):
+            os.mkdir(targets[i])
+
+    args_list = [(on, file, targets) for file in listdir(data_set_path)]
+    pool = mp.Pool(pool_size)
+    pool.map(unpack_split_file, args_list)
+
+
+def unpack_split_file(args):
+    split_table_file(*args)
+
+
+def split_table_file(on, table_file_path, split_target_dir_mapping):
+    """
+    Splits a table stored in a file on a specified column, writing it to the
+    :param on: The column of the table to split on
+    :param table_file_path: Full path to the file containing the table to split
+    :param split_target_dir_mapping: A mapping from bucket/split number to output directory to write results
+    :return: None
+    """
+    table_file_name = os.path.split(table_file_path)[1]
+    logger.debug("Reading: %s" % table_file_name)
+    df = pd.read_csv(table_file_path, engine='python')
+    logger.debug("Splitting: %s" % table_file_name)
+    df['bucket'] = df[on].apply(get_bucket)
+    for i in range(num_splits):
+        output_file = os.path.join(split_target_dir_mapping[i], table_file_name)
+        df[df['bucket'] == i].drop('bucket', axis=1).to_csv(output_file, index=False, compression='gzip' if compress else None)
+
+
+def create_target_directories():
+    global target_directories
+    target_directories = {i: os.path.join(output_directory, "%05d" % i) for i in range(num_splits)}
+    for i in target_directories:
+        target_dir = target_directories[i]
+        if os.path.isfile(target_dir):
+            logger.error("File exists: %s" % target_dir)
+            exit(1)
+        if not os.path.isdir(target_dir):
+            os.mkdir(target_dir)  # create it if it doesn't exist
+    return target_directories
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Split the Reddit data-set", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    io_options_group = parser.add_argument_group("I/O Options")
+    io_options_group.add_argument('-in', "--input", help="Input directory")
+    io_options_group.add_argument('-out', "--output", help="Output directory")
+    io_options_group.add_argument('-i', "--include", narge='+',help="Sub-Directory to process")
+    io_options_group.add_argument('-x', '--exclude', nargs='+', help="Exclude part of the data set")
+    io_options_group.add_argument('-c', '--compress', action='store_true', help='Compress output')
+
+    io_options_group.add_argument('--submissions', action='store_true', hel='Split by submission')
+
+    options_group = parser.add_argument_group("Options")
+    options_group.add_argument('-n', '--num-splits', type=int, default=1024, help="Number of ways to split data set")
+    options_group.add_argument('-p', '--pool-size', type=int, default=20, help="Thread-pool size")
+    options_group.add_argument('-on', '--on', type=str, default="user_id", help="Field to split on")
+
+    console_options_group = parser.add_argument_group("Console Options")
+    console_options_group.add_argument('-v', '--verbose', action='store_true', help='verbose output')
+    console_options_group.add_argument('--debug', action='store_true', help='Debug Console')
+    console_options_group.add_argument('-log', '--log', type=str, default=None, help="Logging file")
+
+    args = parser.parse_args()
+
+    global logger
+    if args.debug:
+        logging.basicConfig(filename=args.log, format='[%(asctime)s][%(levelname)s][%(funcName)s] - %(message)s')
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.DEBUG)
+
+    elif args.verbose:
+        logging.basicConfig(filename=args.log, format='[%(asctime)s][%(levelname)s][%(funcName)s] - %(message)s')
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.INFO)
+    else:
+        logging.basicConfig(filename=args.log, format='[log][%(levelname)s] - %(message)s')
+        logger = logging.getLogger(__name__)
+        logger.setLevel(logging.WARNING)
+
+    return args
+
+
+def main():
+    args = parse_args()
+
+    global input_directory, output_directory, num_splits, pool_size, compress
+    input_directory = args.input
+    output_directory = args.output
+    num_splits = args.num_splits
+    pool_size = args.pool_size
+    compress = args.compress
+
+    logger.debug("Input directory: %s" % input_directory)
+    if os.path.isfile(input_directory)or not os.path.isdir(input_directory):
+        logger.error("Not a directory: %s" % input_directory)
+        raise Exception()
+
+    if not os.path.exists(args.output):
+        logger.debug("Output directory: %s did not exist. Creating it..." % output_directory)
+        os.makedirs(output_directory)
+    else:
+        logger.debug("Output directory: %s" % output_directory)
+
+    split_all_data_sets(args.on, include=args.include, exclude=args.exclude)
+
+
+if __name__ == "__main__":
+    main()
