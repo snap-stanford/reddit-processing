cd996ea0eea63de70e841c6d92075998a4a60977
Jon Deaton <jonpauldeaton@gmail.com>
2018-03-25 18:20:58 -0700

    Auto push

--- scripts/performance_test.py
+++ scripts/performance_test.py
@@ -11,14 +11,28 @@ import snap
 
 perm_set = '1234567'
 
+
 def test_dict_insert(d):
     for p in [''.join(p) for p in permutations(perm_set)]:
         d[p.encode()] = p.encode()
+
 
 def test_dict_lookup(d):
     for p in [''.join(p) for p in permutations(perm_set)]:
         if p.encode() in d:
             pass
+
+
+def test_it(d, name, norm_insert, norm_lookup):
+    t = time.time()
+    test_dict_insert(d)
+    t_insert = time.time() - t
+    print("%s insert: %s (%s)" % (name, t_insert, t_insert / norm_insert))
+
+    t = time.time()
+    test_dict_lookup(d)
+    t_lookup = time.time() - t
+    print("%s lookup: %s (%s)" % (name, t_lookup, t_lookup / norm_lookup))
 
 def performance_test():
 
@@ -26,59 +40,26 @@ def performance_test():
     d = {}
     t = time.time()
     test_dict_insert(d)
-    print("dict insert: %s" % (time.time() - t))
+    norm_insert = time.time() - t
+    print("dict insert: %s" % norm_insert)
 
     t = time.time()
     test_dict_lookup(d)
-    print("dict lookup: %s" % (time.time() - t))
-
+    norm_lookup = time.time() - t
+    print("dict lookup: %s" % norm_lookup)
 
     # Shared memory hash table
     if sys.version_info < (3, 0):
         d = HashTable("table", capacity=1000000)
     else:
         d = HashTable(key_type=ctypes.c_char_p, value_type=ctypes.c_char_p, capacity=1000000)
-    t = time.time()
-    test_dict_insert(d)
-    print("Shared-Memory HashMap insert: %s" % (time.time() - t))
+    test_it(d, "Shared-Memory HashTable", norm_insert, norm_lookup)
 
-    t = time.time()
-    test_dict_lookup(d)
-    print("Shared-Memory HashMap lookup: %s" % (time.time() - t))
+    test_it(snap.TStrStrH(), "SNAP THash", norm_insert, norm_lookup)
+    test_it(mp.Manager().dict(), "mp.Manager.dict", norm_insert, norm_lookup)
 
-
-    # SNAP THash
-    d = snap.TStrStrH()
-    t = time.time()
-    test_dict_insert(d)
-    print("SNAP THash insert: %s" % (time.time() - t))
-
-    t = time.time()
-    test_dict_lookup(d)
-    print("SNAP THash lookup: %s" % (time.time() - t))
-
-
-    # Multiprocessing Manager
-    d = mp.Manager().dict()
-    t = time.time()
-    test_dict_insert(d)
-    print("Manager().dict insert: %s" % (time.time() - t))
-
-    t = time.time()
-    test_dict_lookup(d)
-    print("Manager().dict lookup: %s" % (time.time() - t))
-
-
-    # Gnu dbm database
     db = gdbm.open('/lfs/madmax3/0/jdeaton/dbm_cache/cache', 'c')
-    t = time.time()
-    test_dict_insert(db)
-    print("dbm insert time: %s" % (time.time() - t))
-
-    t = time.time()
-    test_dict_lookup(db)
-    print("dbm lookup time: %s" % (time.time() - t))
-
+    test_it(db, "dbm", norm_insert, norm_lookup)
     db.close()
 
 
